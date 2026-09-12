02f31141af30751711ce85935ed89757c59557f6
Jiří Beneš <mail@jiribenes.com>
2026-09-12 16:48:23 +0200

    Migrate to new effekt-nix API (#3)

--- flake.nix
+++ flake.nix
@@ -3,59 +3,69 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     effekt-nix = {
       url = "github:jiribenes/effekt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows ="flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, effekt-nix }:
-    ## If you want only some specific systems, do the following instead:
-    # flake-utils.lib.eachSystem ["aarch64-linux" "aarch64-darwin"] (system:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        effekt-lib = effekt-nix.lib.${system};
+  outputs = { self, nixpkgs, effekt-nix }:
+    let
+      ## Builds for all systems supported by effekt-nix.
+      ## If you want only some specific systems, do the following instead:
+      # systems = ["aarch64-linux" "aarch64-darwin"];
+      systems = effekt-nix.lib.supportedSystems;
 
-        ## Project configuration
-        pname = "nette-effekte";            # package name
-        version = "0.1.0";                  # package version
-        mainFile = "app/main.effekt";       # relative path to entrypoint (as a string)
-        testFiles = [ "test/main.effekt" ]; # relative paths to tests (as a string)
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-        ## Effekt configuration
-        effektConfig = {
+      ## Project configuration
+      pname = "nette-effekte";         # package name
+      version = "0.1.0";                 # package version
+      mainFile = "app/main.effekt";      # relative path to entrypoint (as a string)
+      testFiles = [ "test/main.effekt" ]; # relative paths to tests (as a string)
+
+      ## Effekt configuration
+      effektConfig = {
           ## Uncomment and set a specific version if needed:
           # version = "0.10.0";
 
           ## Select the backends that your project works on:
-          backends = with effekt-lib.effektBackends; [ js ];
+          backends = bs: with bs; [ js ];
         };
+    in {
+      packages = forAllSystems (system:
+        let
+          effekt-lib = effekt-nix.lib.mkLib nixpkgsFor.${system};
 
-        # Chooses the correct Effekt package.
-        effektBuild = effekt-lib.getEffekt effektConfig;
-      in {
-        packages.default = effekt-lib.buildEffektPackage {
-          inherit pname version;
-          src = ./.;
-          main = mainFile;
-          tests = testFiles;
-          extraEffektFlags = [ "--no-optimize" ];
+          # Chooses the correct Effekt package.
+          effektBuild = effekt-lib.getEffekt effektConfig;
+        in {
+          default = effekt-lib.buildEffektPackage {
+            inherit pname version;
+            src = ./.;
+            main = mainFile;
+            tests = testFiles;
+            extraEffektFlags = [ "--no-optimize" ];
 
-          effekt = effektBuild;
-          inherit (effektConfig) backends;
-        };
+            effekt = effektBuild;
+            inherit (effektConfig) backends;
+          };
+        }
+      );
 
-        devShells.default = effekt-lib.mkDevShell {
-          effekt = effektBuild;
-        };
+      devShells = forAllSystems (system:
+        let
+          effekt-lib = effekt-nix.lib.mkLib nixpkgsFor.${system};
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.default;
-          name = pname;
-        };
-      }
-    );
+          # Chooses the correct Effekt package.
+          effektBuild = effekt-lib.getEffekt effektConfig;
+        in {
+          default = effekt-lib.mkDevShell {
+            effekt = effektBuild;
+            inherit (effektConfig) backends;
+          };
+        }
+      );
+    };
 }
